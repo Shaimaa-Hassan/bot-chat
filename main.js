824da4ae15fa9f383b9daa3ebe1ea714fcfad8ab
Shaimaa-Hassan <shaimaa.elsaeed@gmail.com>
2022-05-30 16:08:05 +0300

    bot

---
+++ main.js
@@ -0,0 +1,33 @@
+let form = document.querySelector("form");
+let chatArea = document.querySelector("#chat-area");
+let message = document.querySelector("#message");
+// let send = document.getElementById("send");
+// i used fetch
+async function getbotanswer(){
+    const responseText = await fetch("bot-answer.json");//اسم ملف الجسون اللي عندي وبنحط اي لينك لو عايز خارجي يكون فيه اوبجيكت
+    const data = await responseText.json();// بنحوله من نص لاوبجيكت
+    // console.log(data);
+    // console.log(responseText);
+    form.onsubmit = (e)=>{//يعني لما ادوس على انتر او الزرار
+        e.preventDefault();
+        // console.log("ok");
+        if(message.value.trim() === ""){
+            alert("please insert value");
+        }
+        else{
+            data.forEach((item)=>{
+                if(message.value.trim().toLowerCase() === item.my_msg){
+                    chatArea.innerHTML +=`<p class=" p-2 " style="background-color: #ddd;">${message.value}</p>  `
+                    let timeres = Math.floor(Math.random()*10000);
+                    console.log(timeres);//هيطبع الوقت اللي هتتعرض فيه الرسالة 
+                    setTimeout(()=>{chatArea.innerHTML += `  <p class="bg-info p-2">${item.bot_answer}</p>`}, timeres);
+                }
+            })
+            // if(message.value.trim() === (data[0].my_msg)){
+            //     console.log(data[0].bot_answer);
+            // }
+        }
+    }
+}
+getbotanswer();
+
